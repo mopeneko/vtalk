26707a04485eca42899b8a3c3d7c1208b166ada9
mopeneko <26901342+mopeneko@users.noreply.github.com>
2020-07-13 23:09:45 +0900

    Create default layout

---
+++ frontend/components/VtalkHeader.vue
@@ -0,0 +1,22 @@
+<template>
+  <header>
+    <v-app-bar
+      app
+      hide-on-scroll
+    >
+      <v-app-bar-nav-icon @click="isEnabled = !isEnabled" />
+      <v-toolbar-title>ブイトーーク！</v-toolbar-title>
+    </v-app-bar>
+
+    <vtalk-menu :is-enabled="isEnabled" />
+  </header>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from "vue-property-decorator";
+
+@Component
+export default class VtalkHeader extends Vue {
+  isEnabled: Boolean = false;
+}
+</script>
---
+++ frontend/components/VtalkMenu.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-navigation-drawer
+    v-model="isEnabled"
+    app
+  />
+</template>
+
+<script lang=ts>
+import { Vue, Component, Prop } from "vue-property-decorator";
+
+@Component
+export default class VtalkMenu extends Vue {
+  @Prop({ type: Boolean, required: true }) readonly isEnabled!: Boolean;
+}
+</script>
